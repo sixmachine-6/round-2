62ec9e96d72a3496170fd764698ebb7d7b2777f8
sixmachine-6 <126985098+sixmachine-6@users.noreply.github.com>
2024-05-19 11:23:25 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,13 @@
+const button6 = document.querySelector("#info");
+const button7 = document.querySelector("#info1");
+
+console.log(button6);
+button6.addEventListener("click", function () {
+  window.location.href =
+    "https://www.berkshirehathaway.com/meet01/visguide2024.pdf";
+});
+console.log(button7);
+button7.addEventListener("click", function () {
+  window.location.href =
+    "https://www.berkshirehathaway.com/meet01/2024proxystmt.pdf";
+});
---
+++ stock.js
@@ -0,0 +1,13 @@
+const button6 = document.querySelector("#info");
+const button7 = document.querySelector("#info1");
+
+console.log(button6);
+button6.addEventListener("click", function () {
+  window.location.href =
+    "https://www.berkshirehathaway.com/brkshareholderinfo/compab.pdf";
+});
+console.log(button7);
+button7.addEventListener("click", function () {
+  window.location.href =
+    "https://www.berkshirehathaway.com/brkshareholderinfo/transferagentinfo.pdf";
+});
