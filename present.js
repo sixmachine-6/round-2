04f451a1204cb73711946e5412f0acbd1ffdcc94
sixmachine-6 <126985098+sixmachine-6@users.noreply.github.com>
2024-05-19 11:22:41 +0530

    Add files via upload

---
+++ present.js
@@ -0,0 +1,13 @@
+const button6 = document.querySelector("#info");
+const button7 = document.querySelector("#info1");
+
+console.log(button6);
+button6.addEventListener("click", function () {
+  window.location.href =
+    "https://www.berkshirehathaway.com/bhenergy/BHE2022InvestPresent.pdf";
+});
+console.log(button7);
+button7.addEventListener("click", function () {
+  window.location.href =
+    "https://www.berkshirehathaway.com/bhenergy/BHE2021InvestPresent.pdf";
+});
---
+++ subs.js
@@ -0,0 +1,13 @@
+const button6 = document.querySelector("#info");
+const button7 = document.querySelector("#info1");
+
+console.log(button6);
+button6.addEventListener("click", function () {
+  window.location.href =
+    "https://www.berkshirehathaway.com/sustainability/berkshire2022eeo1.pdf";
+});
+console.log(button7);
+button7.addEventListener("click", function () {
+  window.location.href =
+    "https://www.berkshirehathaway.com/sustainability/berkshire2021eeo1.pdf";
+});
