93a9ef34399ba434d1b89455e4ad768855b52d46
sixmachine-6 <126985098+sixmachine-6@users.noreply.github.com>
2024-05-19 11:21:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#report");
+const button3 = document.querySelector("#celebration");
+const button4 = document.querySelector("#store");
+const button5 = document.querySelector("#meeting");
+const button6 = document.querySelector("#stock");
+const button7 = document.querySelector("#sec");
+const button8 = document.querySelector("#subs");
+const button9 = document.querySelector("#present");
+const button10 = document.querySelector("#warren");
+const button11 = document.querySelector("#charlie");
+const button12 = document.querySelector("#facts");
+const button13 = document.querySelector("#wesco");
+const button14 = document.querySelector("#holder");
+const button15 = document.querySelector("#company");
+const button16 = document.querySelector("#document");
+const button17 = document.querySelector("#news");
+
+button1.addEventListener("click", function () {
+  window.location.href = "message.html";
+});
+console.log(button2);
+
+button2.addEventListener("click", function () {
+  window.location.href = "reports.html";
+});
+
+button3.addEventListener("click", function () {
+  window.location.href = "https://www.ebay.com/itm/185860708853";
+});
+
+button4.addEventListener("click", function () {
+  window.location.href = "https://berkshirewear.com/";
+});
+
+button5.addEventListener("click", function () {
+  window.location.href = "meeting.html";
+});
+
+button6.addEventListener("click", function () {
+  window.location.href = "stock.html";
+});
+
+button7.addEventListener("click", function () {
+  window.location.href =
+    "https://www.sec.gov/cgi-bin/browse-edgar?company=berkshire+hathaway&match=&CIK=&filenum=&State=&Country=&SIC=&owner=exclude&Find=Find+Companies&action=getcompany";
+});
+
+button8.addEventListener("click", function () {
+  window.location.href = "subs.html";
+});
+
+button9.addEventListener("click", function () {
+  window.location.href = "present.html";
+});
+
+button10.addEventListener("click", function () {
+  window.location.href =
+    "https://www.berkshirehathaway.com/SpecialLetters/WEB%20past%20present%20future%202014.pdf";
+});
+
+button11.addEventListener("click", function () {
+  window.location.href =
+    "https://www.berkshirehathaway.com/SpecialLetters/CTM%20past%20present%20future%202014.pdf";
+});
+
+button12.addEventListener("click", function () {
+  window.location.href = "https://www.berkshirehathaway.com/activisionltr.pdf";
+});
+
+button13.addEventListener("click", function () {
+  window.location.href = "charlie.html";
+});
+
+button14.addEventListener("click", function () {
+  window.location.href = "shareholder.html";
+});
+
+button15.addEventListener("click", function () {
+  window.location.href = "company.html";
+});
+
+button16.addEventListener("click", function () {
+  window.location.href = "documents.html";
+});
+
+button17.addEventListener("click", function () {
+  window.location.href = "news.html";
+});
+
+console.log(button17);
+console.log(button14);
+
+console.log(button13);
+console.log(button3);
